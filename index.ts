2d64a70c055a1963566df802aec72ad698f6464f
champgm <github@champgm.e4ward.com>
2018-12-15 14:10:09 -0500

    let's use freac instead, VLC is doing weird stuff

--- index.ts
+++ index.ts
@@ -17,22 +17,36 @@ doStuff().then(() => {
 });
 
 async function doStuff() {
-  // const vlcUrl = 'https://download.videolan.org/pub/videolan/vlc/3.0.4/win64/vlc-3.0.4-win64.exe';
-  const vlcUrl = 'https://download.videolan.org/pub/videolan/vlc/3.0.4/win64/vlc-3.0.4-win64.zip';
+  // const vlcUrl = 'https://download.videolan.org/pub/videolan/vlc/3.0.4/win64/vlc-3.0.4-win64.zip';
+  const freeAcUrl = 'https://github.com/enzo1982/freac/releases/download/v1.0.32/freac-1.0.32-bin.zip';
   // const vlcFile = 'vlc.exe';
-  const vlcFile = 'vlc.zip';
-  const expectedSize = 41486400;
+  // const vlcFile = 'vlc.zip';
 
-  const stats = fs.statSync(vlcFile);
-  const fileSizeInBytes = stats.size;
-  console.log('Downloading VLC...');
+  // const expectedSize = 70270025;
+  const freeAcFile = 'freac-1.0.32-bin.zip';
+
+  console.log('Checking for VLC...');
+  let fileSizeInBytes;
+  try {
+    const stats = fs.statSync(vlcFile);
+    fileSizeInBytes = stats.size;
+  } catch (error) {
+    console.log("Couldn't find VLC file, will download.");
+  }
   if (fileSizeInBytes !== expectedSize) {
+    console.log(`Existing VLC file is the wrong size,
+expected ${expectedSize},
+ but got ${fileSizeInBytes},
+downloading again...`);
     await getFile2(vlcUrl, vlcFile);
   } else {
     console.log('VLC file seems to be the right size already, no need to download.');
   }
 
-  await unzipFile(vlcFile, '.');
+  // await unzipFile(vlcFile, '.');
+
+  console.log('Unzipping...');
+  await execPromise(`unzip -o ${vlcFile} -d .`);
 
   const asdf = await execPromise('getCdDriveLetter.bat');
   console.log(`ADF: ${asdf}`);
@@ -62,9 +76,12 @@ function getFile2(vlcUrl, fileName) {
   });
 }
 
-function unzipFile(zipFile, destination) {
+async function unzipFile(zipFile, destination) {
+  console.log('Unzipping VLC file...');
   const stream = fs.createReadStream(zipFile).pipe(unzipper.Extract({ path: destination }));
-  return makePromiseStream(stream);
+  // await makePromiseStream(stream);
+
+  console.log('Extracted.');
 }
 
 function getFile(vlcUrl, fileName): Promise<any> {
@@ -76,7 +93,7 @@ function getFile(vlcUrl, fileName): Promise<any> {
 function makePromiseStream(stream) {
   return new Promise(async (resolve, reject) => {
     try {
-      stream.on('finish', () => {
+      stream.on('close', () => {
         resolve();
       });
     } catch (error) {
